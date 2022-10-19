294b86f374fabdf89eecd92df61a20e47cd45957
Day Nights <116160740+DayNightss@users.noreply.github.com>
2022-10-19 11:06:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+#Autors: Vladislavs Sereda
+#Datums: 19/10/2022
+# Programma, kas sasveicinās ar lietotāju
+print ("Hello Stranger!")
+print ("************************************")
+vards=input("Enter Ur Name And Press Enter:")
+
+print ("UR GOSHDARN RIGHT")
+
+
+print(vards[0])
+if vards[0] == "S" :
+  print ("Tu Esi Slikta Gala")
+elif vards[0] == "S" :
+  print ("Good Job!")
+else:
+  print("You Tried And Failed")
+
+
+
+
+
+
+  
+
+  
